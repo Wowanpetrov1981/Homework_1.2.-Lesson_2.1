4fbecc5662fd0e9ed2c99d3da8f7f877343c15cf
Wowanpetrov1981 <petrovik@list.ru>
2022-10-24 09:11:33 +0500

    Исправил проверки

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,4 @@
+
 public class Car {
     String brand;
     String model;
@@ -5,43 +6,17 @@ public class Car {
     String color;
     int productionYear;
     String productionCountry;
-    String defaultValue = "default";
-    double defaultEngineVolume = 1.5;
-    String defaultColor = "белый";
-    int defaultProductionYear = 2000;
 
     Car(String brand, String model, int productionYear, String productionCountry, String color, double engineVolume) {
-        if (brand != "") {
-            this.brand = brand;
-        } else {
-            this.brand = defaultValue;
-        }
-        if (model != "") {
-            this.model = model;
-        } else {
-            this.model = defaultValue;
-        }
-        if (productionCountry != "") {
-            this.productionCountry = productionCountry;
-        } else {
-            this.productionCountry = defaultValue;
-        }
-        if (engineVolume > 0) {
-            this.engineVolume = engineVolume;
-        } else {
-            this.engineVolume = defaultEngineVolume;
-        }
-        if (color != "") {
-            this.color = color;
-        } else {
-            this.color = defaultColor;
-        }
-        if (productionYear > 0) {
-            this.productionYear = productionYear;
-        } else {
-            this.productionYear = defaultProductionYear;
-        }
+        this.brand = CarService.validationCheck(brand, "default");
+        this.model = CarService.validationCheck(model, "default");
+        this.productionCountry = CarService.validationCheck(productionCountry, "default");
+        this.engineVolume = engineVolume > 0.0f ? engineVolume : 1.5f;
+        this.color = CarService.validationCheck(color, "белый");
+        this.productionYear = productionYear > 0 ? productionYear : 2000;
     }
+
+
     void informationAboutTheCar() {
         System.out.println(brand + " " + model + ", " + productionYear + " год выпуска, страна сборки - " + productionCountry + ", цвет - " + color + ", объем двигателя — " + engineVolume + " л.");
 
--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ public class Main {
         System.out.println("Домашнее задание 1 задание 3 к уроку 2.1");
         System.out.println();
 
-        Car lada = new Car("Lada ", "Granta", 2015, "Россия", "желтый", 1.7);
+        Car lada = new Car("Lada", "Granta", 2015, "Россия", "желтый", 1.7);
         lada.informationAboutTheCar();
-        Car audi = new Car("Audi ", "A8 50 L TDI quattro", 2020, "Германия", "черный", 3.0);
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", "черный", 3.0);
         audi.informationAboutTheCar();
-        Car bmw = new Car("BMW ", "Z8", 2021, "Германия", "черный", 3.0);
+        Car bmw = new Car("BMW", "Z8", 2021, "Германия", "черный", 3.0);
         bmw.informationAboutTheCar();
-        Car kia = new Car("Kia ", "Sportage 4-го поколения", 2018, "Южная Корея", "красный", 2.4);
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2018, "Южная Корея", "красный", 2.4);
         kia.informationAboutTheCar();
-        Car hyundai = new Car("Hyundai ", "Avante", 2016, "Южная Корея", "оранжевый", 1.6);
+        Car hyundai = new Car("Hyundai", "Avante", 2016, "Южная Корея", "оранжевый", 1.6);
         hyundai.informationAboutTheCar();
     }
 }
